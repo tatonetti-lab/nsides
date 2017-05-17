cef713455d98724456e9628f3ce821eab9e10bc4
rvanguri <rvanguri@brokkr.cpmc.columbia.edu>
2017-05-17 12:17:51 -0400

    get outcomes data

--- condor/get_data.py
+++ condor/get_data.py
@@ -1,5 +1,10 @@
 import wget
 
+print "Downloading drug exposures..."
 for i in range(0,55):
     url = 'http://stash.osgconnect.net/+rvanguri/AEOLUS_all_reports_'+str(i)+'.npy'
     print wget.download(url)
+
+print "Downloading outcomes..."
+url = 'http://stash.osgconnect.net/+rvanguri/AEOLUS_all_reports_alloutcomes.mtx'
+print wget.download(url)
