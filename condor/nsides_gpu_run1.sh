166819057c7839f75d694bbfd5ef66ed2e136489
Rami Vanguri <rvanguri@login01.osgconnect.net>
2017-05-23 01:12:38 -0500

    log file redirect

--- condor/nsides_gpu_run1.sh
+++ condor/nsides_gpu_run1.sh
@@ -20,16 +20,16 @@ echo
 /usr/local/bin/pip install --user keras
 
 # Run the scripts
-python prepare_data.py --model-num $1 > prepare_data.log
-python prepare_data_separate_reports.py --model-num $1 > prepare_data_separate_reports.log
-python mlp_dnn.py --model-number $1 > mlp_dnn.log
-python mlp_shallow.py --run-comparisons --model-number $1 > mlp_shallow.log
-python eval_model.py --model-type tflr --model-number $1 > eval_model_tflr.log
-python eval_model.py --model-type bdt --model-number $1 > eval_model_bdt.log
-python eval_model.py --model-type rfc --model-number $1 > eval_model_rfc.log
-python eval_model.py --model-type lrc --model-number $1 > eval_model_lrc.log
-python eval_model.py --model-type dnn --model-number $1 > eval_model_dnn.log
-python eval_model.py --model-type nopsm --model-number $1 > eval_model_nopsm.log
+python prepare_data.py --model-num $1 | tee prepare_data.log
+python prepare_data_separate_reports.py --model-num $1 | tee prepare_data_separate_reports.log
+python mlp_dnn.py --model-number $1 | tee mlp_dnn.log
+python mlp_shallow.py --run-comparisons --model-number $1 | tee mlp_shallow.log
+python eval_model.py --model-type tflr --model-number $1 | tee eval_model_tflr.log
+python eval_model.py --model-type bdt --model-number $1 | tee eval_model_bdt.log
+python eval_model.py --model-type rfc --model-number $1 | tee eval_model_rfc.log
+python eval_model.py --model-type lrc --model-number $1 | tee eval_model_lrc.log
+python eval_model.py --model-type dnn --model-number $1 | tee eval_model_dnn.log
+python eval_model.py --model-type nopsm --model-number $1 | tee eval_model_nopsm.log
 
 # Clean up the data on the remote machine 
 rm -rf *.npy
