442519256beec0925552b71f23e5f979b1b023f4
Joseph D. Romano <jdromano2@gmail.com>
2017-05-30 17:44:54 -0400

    Finish writing slurm GPU runner script - next up is testing

--- condor/nsides_habanero_slurm.sh
+++ condor/nsides_habanero_slurm.sh
@@ -12,21 +12,45 @@
 #SBATCH -o nsides_results_%A_%a.out	 # Standard output redirection
 #SBATCH -e nsides_results_%A_%a.err	 # Standard error redirection
 
+start=`date +%s`
+
 module load cuda80/toolkit cuda80/blas cudnn/5.1
 module load anaconda/2-4.2.0
 
 pip install --user tensorflow-gpu keras h5py
 
-python prepare_data.py --model-number ${SLURM_ARRAY_TASK_ID} | tee prepare_data.log
-python prepare_data_separate_reports.py --model-number ${SLURM_ARRAY_TASK_ID} | tee prepare_data_separate_reports.log
-python mlp_dnn.py --model-number ${SLURM_ARRAY_TASK_ID} | tee mlp_dnn.log
-python mlp_shallow.py --run-comparisons --model-number ${SLURM_ARRAY_TASK_ID} | tee mlp_shallow.py
-python eval_model.py --model-type tflr --model-number ${SLURM_ARRAY_TASK_ID} | tee eval_model_tflr.log
-python eval_model.py --model-type bdt --model-number ${SLURM_ARRAY_TASK_ID} | tee eval_model_bdt.log
-python eval_model.py --model-type rfc --model-number ${SLURM_ARRAY_TASK_ID} | tee eval_model_rfc.log
-python eval_model.py --model-type lrc --model-number ${SLURM_ARRAY_TASK_ID} | tee eval_model_lrc.log
-python eval_model.py --model-type dnn --model-number ${SLURM_ARRAY_TASK_ID} | tee eval_model_dnn.log
-python eval_model.py --model-type nopsm --model-number ${SLURM_ARRAY_TASK_ID} | tee eval_model_nopsm.log
+i=${SLURM_ARRAY_TASK_ID}
 
-rm -rf *.npy
-tar -czvf nsides_results_$1.tgz results*.pkl *.log
+# Note: Data files are not checked into git, and are located in `data/`, relative to the working directory
+python prepare_data.py --model-number $i | tee prepare_data.log
+python prepare_data_separate_reports.py --model-number $i | tee prepare_data_separate_reports.log
+
+# Run the model
+python mlp_dnn.py --model-number $i | tee mlp_dnn.log
+python mlp_shallow.py --run-comparisons --model-number $i | tee mlp_shallow.log
+
+# Evaluate the model's performance
+python eval_model.py --model-type tflr --model-number $i | tee eval_model_tflr.log
+python eval_model.py --model-type bdt --model-number $i | tee eval_model_bdt.log
+python eval_model.py --model-type rfc --model-number $i | tee eval_model_rfc.log
+python eval_model.py --model-type lrc --model-number $i | tee eval_model_lrc.log
+python eval_model.py --model-type dnn --model-number $i | tee eval_model_dnn.log
+python eval_model.py --model-type nopsm --model-number $i | tee eval_model_nopsm.log
+
+# Clean up
+printf -v j "%03d" $i
+tar -czvf ../../nsides_results/results_"$j"_gpu.tar.gz results*.pkl *.log
+
+rm ./*.npy
+rm ./*.log
+rm ./*.mtx
+rm ./*.out
+rm ./*.err
+rm ./*.tgz
+rm ./results_*
+
+end=`date +%s`
+
+runtime=$((end-start))
+
+echo Elapsed time for running model $i : $runtime
