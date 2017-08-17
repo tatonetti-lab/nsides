de905195464ee29b9cadc25f49d3c72fe5c4ee7b
Rami Vanguri <r.vanguri@columbia.edu>
2017-08-17 17:32:55 -0400

    ingredient level option

--- condor/get_data.py
+++ condor/get_data.py
@@ -5,12 +5,24 @@ for i in range(0,55):
     url = 'http://stash.osgconnect.net/+rvanguri/AEOLUS_all_reports_'+str(i)+'.npy'
     print wget.download(url)
 
+for i in range(0,55):
+    url = 'http://stash.osgconnect.net/+rvanguri/AEOLUS_all_reports_IN_'+str(i)+'.npy'
+    print wget.download(url)
+
 print "Downloading outcomes..."
 url = 'http://stash.osgconnect.net/+rvanguri/AEOLUS_all_reports_alloutcomes.mtx'
 print wget.download(url)
 
+print "Downloading outcomes..."
+url = 'http://stash.osgconnect.net/+rvanguri/AEOLUS_all_reports_IN_alloutcomes.mtx'
+print wget.download(url)
+
 print "Downloading report ids..."
 url = 'http://stash.osgconnect.net/+rvanguri/all_reportids.npy'
+print wget.download(url)
+
+print "Downloading IN report ids..."
+url = 'http://stash.osgconnect.net/+rvanguri/all_reportids_IN.npy'
 print wget.download(url)
 
 print "Downloading age vector..."
